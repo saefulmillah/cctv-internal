7daf96b1933a64a54aceff5912e434a63e77f6fd
root <root@operasi.localdomain>
2023-01-06 13:28:22 +0700

    add ip public

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const HKTOLL_URL = "http://192.168.66.4:6660/";
+export const HKTOLL_URL = "https://apps.hk-opt.com/";
 export const ANT_URL = "http://192.168.66.24:5080/";
 export const API_URL = "http://localhost:3330/";
 const month = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
